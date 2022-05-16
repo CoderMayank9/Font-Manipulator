3e150fe7e5cc5e07aefed9e193b897cce574916d
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-05-16 18:29:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,4 +2,26 @@ function setup()
 {
 canvas=createCanvas(500,500);
 canvas.position(560,150);
+video=createCapture(VIDEO);
+video.size(550,500);
+posenet=ml5.poseNet(video,modelloaded);
+posenet.on("pose", gotposes);
+}
+function modelloaded()
+{
+console.log("Model is initialized");
+}
+function gotposes(results)
+{
+if(results.length > 0)
+{
+console.log(results);
+}
+}
+function draw()
+{
+background("#33FFFC");
+textSize(50);
+fill("#EC13D2");
+text("Mayank",100,150);
 }
