563de2114087f0b288a424b5bb50f9a731ccca18
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-05-18 18:21:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+rightWristX=0;
+leftWristX=0;
+difference=0;
 function setup()
 {
 canvas=createCanvas(500,500);
@@ -16,12 +19,16 @@ function gotposes(results)
 if(results.length > 0)
 {
 console.log(results);
+rightWristX=results[0].pose.rightWrist.x;
+leftWristX=results[0].pose.leftWrist.x;
+difference=floor(leftWristX-rightWristX);
 }
 }
 function draw()
 {
 background("#33FFFC");
-textSize(50);
+textSize(difference);
 fill("#EC13D2");
 text("Mayank",100,150);
+document.getElementById("fontsize").innerHTML="Font size of the text will be "+ difference + "px";
 }
